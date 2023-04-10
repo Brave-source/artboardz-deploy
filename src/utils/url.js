a62525f778931a950da191b8e4726203fd7da39a
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-10 04:56:03 +0100

    working on improving url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,8 +1,8 @@
-const dev = "http://localhost:3000";
+const dev = "https://admin.artboardz.net";
 const prod = "http://ec2-34-221-201-175.us-west-2.compute.amazonaws.com:3000";
-
-export const baseURL = "https://admin.artboardz.net"
-//   window.location.hostname.split(":")[0] === "localhost" ||
-//   window.location.hostname.includes("192")
-//     ? dev
-//     : prod;
+// console.log(window.location.hostname)
+export const baseURL = "https://admin.artboardz.net";
+  window.location.hostname.split(":")[0] === "localhost" ||
+  window.location.hostname.includes("192")
+    ? dev
+    : prod;
