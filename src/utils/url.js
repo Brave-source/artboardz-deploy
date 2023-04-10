3a6596af3e088f324b25deb70bd230ac7834f0fc
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-10 03:19:00 +0100

    change url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,7 +1,7 @@
 const dev = "http://localhost:3000";
 const prod = "http://ec2-34-221-201-175.us-west-2.compute.amazonaws.com:3000";
 
-export const baseURL = "http://54.197.30.62:3000"
+export const baseURL = "http://ec2-54-197-30-62.compute-1.amazonaws.com:3000"
 //   window.location.hostname.split(":")[0] === "localhost" ||
 //   window.location.hostname.includes("192")
 //     ? dev
