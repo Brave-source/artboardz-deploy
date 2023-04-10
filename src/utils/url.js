5e9054220b7205454128456ef3a29139b391c5ef
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-10 03:54:08 +0100

    change domain name

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,7 +1,7 @@
 const dev = "http://localhost:3000";
 const prod = "http://ec2-34-221-201-175.us-west-2.compute.amazonaws.com:3000";
 
-export const baseURL = "https://54.197.30.62"
+export const baseURL = "https://admin.artboardz.net"
 //   window.location.hostname.split(":")[0] === "localhost" ||
 //   window.location.hostname.includes("192")
 //     ? dev
