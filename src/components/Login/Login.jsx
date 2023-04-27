d995ab4a17af9cb1794a466f0c757de85b601d93
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-27 10:32:35 +0100

    fix: remove url comment

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -45,7 +45,6 @@ async function getStakeAddress(){
         const messageUtf = `account: ${stakeAddrBech32}`;
         const messageHex = Buffer.from(messageUtf).toString("hex");    
         const sigData = await wallet.signData(stakeAddrHex, messageHex);
-        // const res = await axios.post(globalURL == "www" ? `${externalURL}/api/collectors` : `${baseURL}/api/collectors`, sigData);
         const res = await axios.post(globalURL == "www" ? `${externalURL}/api/collections` :`${baseURL}/api/collectors`, sigData);
         dispatch(getAdminSuccess(res.data))
         router.push("/collections")
