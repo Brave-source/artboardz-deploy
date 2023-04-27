be044f2b6414bc27f6e6c64b41ea227f67fdf2d4
Aondohemba Iorolun Emmanuel <emmyaondohemba45@gmail.com>
2023-04-27 06:05:30 +0100

    add new wallet

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -45,7 +45,8 @@ async function getStakeAddress(){
         const messageUtf = `account: ${stakeAddrBech32}`;
         const messageHex = Buffer.from(messageUtf).toString("hex");    
         const sigData = await wallet.signData(stakeAddrHex, messageHex);
-        const res = await axios.post(globalURL == "www" ? `${externalURL}/api/collectors` : `${baseURL}/api/collectors`, sigData);
+        // globalURL == "www" ? `${externalURL}/api/collectors` : `${baseURL}
+        const res = await axios.post(`http:/localhost:3000/api/collectors`, sigData);
         dispatch(getAdminSuccess(res.data))
         router.push("/collections")
     }catch(err){
--- src/pages/api/collectors/index.js
+++ src/pages/api/collectors/index.js
@@ -9,7 +9,8 @@ const registeredUsers = [
     "stake1u8k7mwu8gdqyvgved89996cy6g8d9vw36w7j05qy2etanxgmgl5s7",
     "stake1uynpv0vlulhufm8txwry0da9qq6tn9wn42mxltq65pw403qvdcveh",
     "stake1ux3z54xtearr89w4vcgvhl3zp87sj2kngqurrja764gzpvsj23e49",
-    "stake1uzxtve77gvu44y0q0h4p8y0ag4wjsc7pxhm2gtyjpgftdyg85gad0"
+    "stake1uzxtve77gvu44y0q0h4p8y0ag4wjsc7pxhm2gtyjpgftdyg85gad0",
+    "stake_test1urc8l0tn9vl3dz5ch9lvq05n0a3dd29j6k3l0j9gs8mwrfqf0xx7m"
 ]
 
 export default async function handler (req, res) {
